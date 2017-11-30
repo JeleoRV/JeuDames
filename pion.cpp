53ebe30fe800356105916a1e60ec62f684df6181
GlouglouLand <GlouglouLand>
2017-11-30 11:54:56 +0100

    Fichier pion.cpp terminé

--- pion.cpp
+++ pion.cpp
@@ -4,3 +4,33 @@
 
 using namespace std;
 
+pion::pion(bool _couleur, bool _rang, string _nom) {
+	couleur = _couleur;
+	rang = _rang;
+	nom = _nom;
+}
+
+pion::pion() {
+	rang = PION;
+	couleur = BLANC;
+	nom = "";
+}
+
+pion::pion(bool _couleur) {
+	couleur = _couleur;
+	rang = PION;
+	if (couleur == BLANC) {
+		nom = "b";
+	}
+	else {
+		nom = "n";
+	}
+}
+
+bool pion::promotion() {
+	if (rang == DAME) {
+		return false;
+	}
+	rang = DAME;
+	return true;
+}
