5048777a4045fe9d0f9f566dc2a4ddcbe0aca404
Léo <leo.malbrand@eleves.ec-nantes.fr>
2017-12-07 10:36:21 +0100

    Ajout des tests pour les pions.

--- main_test.cpp
+++ main_test.cpp
@@ -1,1 +1,40 @@
 // main pour les tests googletest
+
+#include <jeu.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+int main(){
+	
+	class pionTest : public ::testing::Test{
+		protected:
+		virtual void SetUp(){
+			dame.promotion();
+		}
+		
+		virtual void TearDown(){
+			//
+		}
+		
+		pion pion_noir = pion(NOIR);
+		pion pion_blanc = pion(BLANC);
+		pion dame = pion();
+	};
+	
+	TEST_F(pionTest, pionCouleur){
+		EXPECT_EQ(NOIR, pion_noir.getCouleur());
+		EXPECT_EQ(BLANC, pion_blanc.getCouleur());
+	}
+	
+	TEST_F(pionTest, pionRang){
+		EXPECT_EQ(DAME, dame.getRang());
+		EXPECT_EQ(false, dame.promotion());
+	}
+	
+	class caseTest : public ::testing::TYest{
+		//
+	};
+	
+	return 0;
+}
