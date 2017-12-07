0eab20965c540c06c4e7dffa73db69c1df381ea4
GlouglouLand <GlouglouLand>
2017-12-07 09:24:46 +0100

    Board.h et jeu.h 1ere modifs

--- board.h
+++ board.h
@@ -2,7 +2,13 @@
 
 // Classe pour le plateau de jeu
 
+#include <vector>
+#include "case.h"
+using namespace std;
+
 class board {
 public:
-	int taille;
+	int taille; // largeur du plateau
+	vector<Case*> cases; // création des différentes cases du plateau
+
 };
