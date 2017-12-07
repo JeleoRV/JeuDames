e4c24d9cd2ef7c2f6993b4ff2419e76a8e6c0e9a
GlouglouLand <GlouglouLand>
2017-12-07 11:43:54 +0100

    Renommage et modification de main_test.cpp en tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,36 @@
+// main pour les tests googletest
+
+#include "jeu.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+	
+class pionTest : public ::testing::Test{
+	protected:
+	virtual void SetUp(){
+		dame.promotion();
+	}
+		
+	virtual void TearDown(){
+		//
+	}
+		
+	pion pion_noir = pion(NOIR);
+	pion pion_blanc = pion(BLANC);
+	pion dame = pion();
+};
+	
+TEST_F(pionTest, pionCouleur){
+	EXPECT_EQ(NOIR, pion_noir.getCouleur());
+	EXPECT_EQ(BLANC, pion_blanc.getCouleur());
+}
+	
+TEST_F(pionTest, pionRang){
+	EXPECT_EQ(DAME, dame.getRang());
+	EXPECT_EQ(false, dame.promotion());
+}
+	
+class caseTest : public ::testing::Test{
+	//
+};
