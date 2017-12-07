100a5eca5ea2e192d4dc506d2c3829ed1d086ec8
Léo <leo.malbrand@eleves.ec-nantes.fr>
2017-12-07 11:55:15 +0100

    Ajout des vecteurs de pions blanc et noir dans la classe jeu et correction du passage par des pointeur de la classe jeu

--- Jeu.cpp
+++ Jeu.cpp
@@ -3,13 +3,18 @@
 using namespace std;
 
 jeu::jeu() { // constructeur par défaut
-	Jeu = true;
-	Victoire = NOIR;
-	Plateau = new board();
+	Jeu = true; // initialisation du booléen Jeu
+	Victoire = NOIR; // Valeaur par défaut
+	NbPion = 0; // pas de pion généré
+	Plateau = new board(); // création avec plateau par défaut
+	JeuBlanc = vector<pion*>(); // pas de pion blanc
+	JeuNoir = vector<pion*>(); // pas de pion noir
+
 }
 
-jeu::jeu(int _taille) {
-	Jeu = true;
-	Victoire = NOIR;
-	Plateau = new board(_taille);
+jeu::jeu(int _taille, int _NbPion) {
+	Jeu = true; // initialisation du bolléen de jeu 
+	Victoire = NOIR; // Valeaur par défaut
+	NbPion = _NbPion; // initialisation du nombre de pion par joueur
+	Plateau = new board(_taille); // initialisation du plateau
 }
--- jeu.h
+++ jeu.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "board.h"
+#include "Vector"
 
 // Création de la classe qui servira a gérer le jeu 
 class jeu {
@@ -8,12 +9,16 @@ class jeu {
 private :
 	bool Jeu; // Booléen donnant l'indication de début et de fin de partie
 	bool Victoire; // Booléen donnant la victoire à un joueur (Noir ou Blanc)
-	board Plateau; // plateau de jeu
+	int NbPion; // Nombre de pion par joueurs au début de la partie
+	board* Plateau; // plateau de jeu
+	vector<pion*> JeuBlanc; // Vecteur contenant les pions du joueur Blanc
+	vector<pion*> JeuNoir;// Vectuer contenant les pions du joueur Noir
+
 
 public :
 	// constructeurs 
 	jeu(); // constructeur par défaut
-	jeu(int _taille); // constructeur de base, avec la taille du plateau comme seul paramètre
+	jeu(int _taille, int _NbPion); // constructeur de base, avec la taille du plateau comme seul paramètre
 
 	// Destructeur 
 	~jeu() {};
