7368946f3c231a6f75082802da2bda0a67a48080
Léo <leo.malbrand@eleves.ec-nantes.fr>
2017-12-07 11:25:22 +0100

    passage par des pointeur sur des plateau dans la classe jeu

--- Jeu.cpp
+++ Jeu.cpp
@@ -5,11 +5,11 @@ using namespace std;
 jeu::jeu() { // constructeur par défaut
 	Jeu = true;
 	Victoire = NOIR;
-	Plateau = board();
+	Plateau = new board();
 }
 
 jeu::jeu(int _taille) {
 	Jeu = true;
 	Victoire = NOIR;
-	Plateau = board(_taille);
+	Plateau = new board(_taille);
 }
